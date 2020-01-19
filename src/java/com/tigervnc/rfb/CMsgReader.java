1ebbe64143fa3467d03ef80006e7bea881687108
Alex Baranowski <alex@euro-linux.com>
2020-01-19 15:49:41 +0100

    els-ASAP auto commit file/tag tigervnc-1.8.0-1.el7.src.rpm

---
+++ src/java/com/tigervnc/rfb/CMsgReader.java
@@ -0,0 +1,430 @@
+/* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright (C) 2011-2017 Brian P. Hinz
+ * Copyright (C) 2017 Pierre Ossman for Cendio AB
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+//
+// CMsgReader - class for reading RFB messages on the client side
+// (i.e. messages from server to client).
+//
+
+package com.tigervnc.rfb;
+
+import java.awt.image.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
+import com.tigervnc.rdr.*;
+
+public class CMsgReader {
+
+  protected CMsgReader(CMsgHandler handler_, InStream is_)
+  {
+    imageBufIdealSize = 0;
+    handler = handler_;
+    is = is_;
+    imageBuf = null;
+    imageBufSize = 0;
+    nUpdateRectsLeft = 0;
+  }
+
+  public void readServerInit()
+  {
+    int width = is.readU16();
+    int height = is.readU16();
+    handler.setDesktopSize(width, height);
+    PixelFormat pf = new PixelFormat();
+    pf.read(is);
+    handler.setPixelFormat(pf);
+    String name = is.readString();
+    handler.setName(name);
+    handler.serverInit();
+  }
+
+  public void readMsg()
+  {
+    if (nUpdateRectsLeft == 0) {
+      int type = is.readU8();
+
+      switch (type) {
+      case MsgTypes.msgTypeSetColourMapEntries:
+        readSetColourMapEntries();
+        break;
+      case MsgTypes.msgTypeBell:
+        readBell();
+        break;
+      case MsgTypes.msgTypeServerCutText:
+        readServerCutText();
+        break;
+      case MsgTypes.msgTypeFramebufferUpdate:
+        readFramebufferUpdate();
+        break;
+      case MsgTypes.msgTypeServerFence:
+        readFence();
+        break;
+      case MsgTypes.msgTypeEndOfContinuousUpdates:
+        readEndOfContinuousUpdates();
+        break;
+      default:
+        throw new Exception("unknown message type");
+      }
+    } else {
+      int x = is.readU16();
+      int y = is.readU16();
+      int w = is.readU16();
+      int h = is.readU16();
+      int encoding = is.readS32();
+
+      switch (encoding) {
+      case Encodings.pseudoEncodingLastRect:
+        nUpdateRectsLeft = 1;     // this rectangle is the last one
+        break;
+      case Encodings.pseudoEncodingXCursor:
+        readSetXCursor(w, h, new Point(x,y));
+        break;
+      case Encodings.pseudoEncodingCursor:
+        readSetCursor(w, h, new Point(x,y));
+        break;
+      case Encodings.pseudoEncodingCursorWithAlpha:
+        readSetCursorWithAlpha(w, h, new Point(x,y));
+        break;
+      case Encodings.pseudoEncodingDesktopName:
+        readSetDesktopName(x, y, w, h);
+        break;
+      case Encodings.pseudoEncodingDesktopSize:
+        handler.setDesktopSize(w, h);
+        break;
+      case Encodings.pseudoEncodingExtendedDesktopSize:
+        readExtendedDesktopSize(x, y, w, h);
+        break;
+      case Encodings.pseudoEncodingClientRedirect:
+        nUpdateRectsLeft = 0;
+        readClientRedirect(x, y, w, h);
+        return;
+      default:
+        readRect(new Rect(x, y, x+w, y+h), encoding);
+        break;
+      };
+
+      nUpdateRectsLeft--;
+      if (nUpdateRectsLeft == 0)
+        handler.framebufferUpdateEnd();
+    }
+  }
+
+  protected void readSetColourMapEntries()
+  {
+    is.skip(1);
+    int firstColour = is.readU16();
+    int nColours = is.readU16();
+    int[] rgbs = new int[nColours * 3];
+    for (int i = 0; i < nColours * 3; i++)
+      rgbs[i] = is.readU16();
+    handler.setColourMapEntries(firstColour, nColours, rgbs);
+  }
+
+  protected void readBell()
+  {
+    handler.bell();
+  }
+
+  protected void readServerCutText()
+  {
+    is.skip(3);
+    int len = is.readU32();
+    if (len > 256*1024) {
+      is.skip(len);
+      vlog.error("cut text too long ("+len+" bytes) - ignoring");
+      return;
+    }
+    byte[] buf = new byte[len];
+    is.readBytes(buf, 0, len);
+    Charset latin1 = Charset.forName("ISO-8859-1");
+    CharBuffer chars = latin1.decode(ByteBuffer.wrap(buf));
+    handler.serverCutText(chars.toString(), len);
+  }
+
+  protected void readFence()
+  {
+    int flags;
+    int len;
+    byte[] data = new byte[64];
+
+    is.skip(3);
+
+    flags = is.readU32();
+
+    len = is.readU8();
+    if (len > data.length) {
+      System.out.println("Ignoring fence with too large payload\n");
+      is.skip(len);
+      return;
+    }
+
+    is.readBytes(data, 0, len);
+
+    handler.fence(flags, len, data);
+  }
+
+  protected void readEndOfContinuousUpdates()
+  {
+    handler.endOfContinuousUpdates();
+  }
+
+  protected void readFramebufferUpdate()
+  {
+    is.skip(1);
+    nUpdateRectsLeft = is.readU16();
+    handler.framebufferUpdateStart();
+  }
+
+  protected void readRect(Rect r, int encoding)
+  {
+    if ((r.br.x > handler.cp.width) || (r.br.y > handler.cp.height)) {
+      vlog.error("Rect too big: "+r.width()+"x"+r.height()+" at "+
+                  r.tl.x+","+r.tl.y+" exceeds "+handler.cp.width+"x"+
+                  handler.cp.height);
+      throw new Exception("Rect too big");
+    }
+
+    if (r.is_empty())
+      vlog.error("Ignoring zero size rect");
+
+    handler.dataRect(r, encoding);
+  }
+
+  protected void readSetXCursor(int width, int height, Point hotspot)
+  {
+    byte pr, pg, pb;
+    byte sr, sg, sb;
+    int data_len = ((width+7)/8) * height;
+    int mask_len = ((width+7)/8) * height;
+    ByteBuffer data = ByteBuffer.allocate(data_len);
+    ByteBuffer mask = ByteBuffer.allocate(mask_len);
+
+    int x, y;
+    byte[] buf = new byte[width*height*4];
+    ByteBuffer out;
+
+    if (width * height == 0)
+      return;
+
+    pr = (byte)is.readU8();
+    pg = (byte)is.readU8();
+    pb = (byte)is.readU8();
+
+    sr = (byte)is.readU8();
+    sg = (byte)is.readU8();
+    sb = (byte)is.readU8();
+
+    is.readBytes(data, data_len);
+    is.readBytes(mask, mask_len);
+
+    int maskBytesPerRow = (width+7)/8;
+    out = ByteBuffer.wrap(buf);
+    for (y = 0;y < height;y++) {
+      for (x = 0;x < width;x++) {
+        int byte_ = y * maskBytesPerRow + x / 8;
+        int bit = 7 - x % 8;
+
+        if ((data.get(byte_) & (1 << bit)) > 0) {
+          out.put(out.position() + 1, pr);
+          out.put(out.position() + 2, pg);
+          out.put(out.position() + 3, pb);
+        } else {
+          out.put(out.position() + 1, sr);
+          out.put(out.position() + 2, sg);
+          out.put(out.position() + 3, sb);
+        }
+
+        if ((mask.get(byte_) & (1 << bit)) > 0)
+          out.put(out.position() + 0, (byte)255);
+        else
+          out.put(out.position() + 0, (byte)0);
+
+        out.position(out.position() + 4);
+      }
+    }
+
+    handler.setCursor(width, height, hotspot, buf);
+  }
+
+  protected void readSetCursor(int width, int height, Point hotspot)
+  {
+    int data_len = width * height * (handler.cp.pf().bpp/8);
+    int mask_len = ((width+7)/8) * height;
+    ByteBuffer data = ByteBuffer.allocate(data_len);
+    ByteBuffer mask = ByteBuffer.allocate(mask_len);
+
+    int x, y;
+    byte[] buf = new byte[width*height*4];
+    ByteBuffer in;
+    ByteBuffer out;
+
+    is.readBytes(data, data_len);
+    is.readBytes(mask, mask_len);
+
+    int maskBytesPerRow = (width+7)/8;
+    in = (ByteBuffer)data.duplicate().mark();
+    out = (ByteBuffer)ByteBuffer.wrap(buf).mark();
+    for (y = 0;y < height;y++) {
+      for (x = 0;x < width;x++) {
+        int byte_ = y * maskBytesPerRow + x / 8;
+        int bit = 7 - x % 8;
+
+        // NOTE: BufferedImage needs ARGB, rather than RGBA
+        if ((mask.get(byte_) & (1 << bit)) != 0)
+          out.put((byte)255);
+        else
+          out.put((byte)0);
+
+        handler.cp.pf().rgbFromBuffer(out, in.duplicate(), 1);
+
+        in.position(in.position() + handler.cp.pf().bpp/8);
+        out.position(out.reset().position() + 4).mark();
+      }
+    }
+
+    handler.setCursor(width, height, hotspot, buf);
+  }
+
+  protected void readSetCursorWithAlpha(int width, int height, Point hotspot)
+  {
+    int encoding;
+
+    PixelFormat rgbaPF =
+      new PixelFormat(32, 32, false, true, 255, 255, 255, 16, 8, 0);
+    ManagedPixelBuffer pb =
+      new ManagedPixelBuffer(rgbaPF, width, height);
+    PixelFormat origPF;
+
+    DataBufferInt buf;
+
+    encoding = is.readS32();
+
+    origPF = handler.cp.pf();
+    handler.cp.setPF(rgbaPF);
+    handler.readAndDecodeRect(pb.getRect(), encoding, pb);
+    handler.cp.setPF(origPF);
+
+    if (pb.getRect().area() == 0)
+      return;
+
+    // ARGB with pre-multiplied alpha works best for BufferedImage
+    buf = (DataBufferInt)pb.getBufferRW(pb.getRect()).getDataBuffer();
+    ByteBuffer bbuf =
+      ByteBuffer.allocate(pb.area()*4).order(rgbaPF.getByteOrder());
+    bbuf.asIntBuffer().put(buf.getData()).flip().mark();
+
+    for (int i = 0;i < pb.area();i++) {
+      byte alpha = bbuf.get(bbuf.position()+3);
+
+      bbuf.put(i*4+3, (byte)(bbuf.get(i*4+2)));
+      bbuf.put(i*4+2, (byte)(bbuf.get(i*4+1)));
+      bbuf.put(i*4+1, (byte)(bbuf.get(i*4+0))); 
+      bbuf.put(i*4+0, (byte)alpha);
+
+      bbuf.position(bbuf.position() + 4);
+    }
+
+    handler.setCursor(width, height, hotspot,
+                      bbuf.array());
+  }
+
+  protected void readSetDesktopName(int x, int y, int w, int h)
+  {
+    String name = is.readString();
+
+    if (x != 0 || y != 0 || w != 0 || h != 0) {
+      vlog.error("Ignoring DesktopName rect with non-zero position/size");
+    } else {
+      handler.setName(name);
+    }
+
+  }
+
+  protected void readExtendedDesktopSize(int x, int y, int w, int h)
+  {
+    int screens, i;
+    int id, flags;
+    int sx, sy, sw, sh;
+    ScreenSet layout = new ScreenSet();
+
+    screens = is.readU8();
+    is.skip(3);
+
+    for (i = 0;i < screens;i++) {
+      id = is.readU32();
+      sx = is.readU16();
+      sy = is.readU16();
+      sw = is.readU16();
+      sh = is.readU16();
+      flags = is.readU32();
+
+      layout.add_screen(new Screen(id, sx, sy, sw, sh, flags));
+    }
+
+    handler.setExtendedDesktopSize(x, y, w, h, layout);
+  }
+
+  protected void readClientRedirect(int x, int y, int w, int h)
+  {
+    int port = is.readU16();
+    String host = is.readString();
+    String x509subject = is.readString();
+
+    if (x != 0 || y != 0 || w != 0 || h != 0)
+      vlog.error("Ignoring ClientRedirect rect with non-zero position/size");
+    else
+      handler.clientRedirect(port, host, x509subject);
+  }
+
+  public int[] getImageBuf(int required) { return getImageBuf(required, 0, 0); }
+
+  public int[] getImageBuf(int required, int requested, int nPixels)
+  {
+    int requiredBytes = required;
+    int requestedBytes = requested;
+    int size = requestedBytes;
+    if (size > imageBufIdealSize) size = imageBufIdealSize;
+
+    if (size < requiredBytes)
+      size = requiredBytes;
+
+    if (imageBufSize < size) {
+      imageBufSize = size;
+      imageBuf = new int[imageBufSize];
+    }
+    if (nPixels != 0)
+      nPixels = imageBufSize / (handler.cp.pf().bpp / 8);
+    return imageBuf;
+  }
+
+  public InStream getInStream() { return is; }
+
+  public int imageBufIdealSize;
+
+  protected CMsgHandler handler;
+  protected InStream is;
+  protected int nUpdateRectsLeft;
+  protected int[] imageBuf;
+  protected int imageBufSize;
+
+  static LogWriter vlog = new LogWriter("CMsgReader");
+}
---
+++ src/java/com/tigervnc/vncviewer/Viewport.java
@@ -0,0 +1,431 @@
+/* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright (C) 2006 Constantin Kaplinsky.  All Rights Reserved.
+ * Copyright (C) 2009 Paul Donohue.  All Rights Reserved.
+ * Copyright (C) 2010, 2012-2013 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2011-2017 Brian P. Hinz
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+//
+// DesktopWindow is an AWT Canvas representing a VNC desktop.
+//
+// Methods on DesktopWindow are called from both the GUI thread and the thread
+// which processes incoming RFB messages ("the RFB thread").  This means we
+// need to be careful with synchronization here.
+//
+
+package com.tigervnc.vncviewer;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.color.ColorSpace;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.Clipboard;
+import java.io.BufferedReader;
+import java.nio.*;
+import javax.swing.*;
+
+import javax.imageio.*;
+import java.io.*;
+
+import com.tigervnc.rfb.*;
+import com.tigervnc.rfb.Cursor;
+import com.tigervnc.rfb.Point;
+
+import static com.tigervnc.vncviewer.Parameters.*;
+
+class Viewport extends JPanel implements MouseListener,
+  MouseMotionListener, MouseWheelListener, KeyListener {
+
+  static LogWriter vlog = new LogWriter("Viewport");
+
+  public Viewport(int w, int h, PixelFormat serverPF, CConn cc_)
+  {
+    cc = cc_;
+    setScaledSize(cc.cp.width, cc.cp.height);
+    frameBuffer = createFramebuffer(serverPF, w, h);
+    assert(frameBuffer != null);
+    setBackground(Color.BLACK);
+
+    cc.setFramebuffer(frameBuffer);
+    OptionsDialog.addCallback("handleOptions", this);
+
+    addMouseListener(this);
+    addMouseWheelListener(this);
+    addMouseMotionListener(this);
+    addKeyListener(this);
+    addFocusListener(new FocusAdapter() {
+      public void focusGained(FocusEvent e) {
+        ClipboardDialog.clientCutText();
+      }
+      public void focusLost(FocusEvent e) {
+        cc.releaseDownKeys();
+      }
+    });
+    setFocusTraversalKeysEnabled(false);
+    setFocusable(true);
+
+    // Send a fake pointer event so that the server will stop rendering
+    // a server-side cursor. Ideally we'd like to send the actual pointer
+    // position, but we can't really tell when the window manager is done
+    // placing us so we don't have a good time for that.
+    cc.writer().writePointerEvent(new Point(w/2, h/2), 0);
+  }
+
+  // Most efficient format (from Viewport's point of view)
+  public PixelFormat getPreferredPF()
+  {
+    return frameBuffer.getPF();
+  }
+
+  // Copy the areas of the framebuffer that have been changed (damaged)
+  // to the displayed window.
+  public void updateWindow() {
+    Rect r = frameBuffer.getDamage();
+    if (!r.is_empty()) {
+      if (image == null)
+        image = (BufferedImage)frameBuffer.getImage();
+      if (cc.cp.width != scaledWidth ||
+          cc.cp.height != scaledHeight) {
+        AffineTransform t = new AffineTransform(); 
+        t.scale((double)scaleRatioX, (double)scaleRatioY);
+        Rectangle s = new Rectangle(r.tl.x, r.tl.y, r.width(), r.height());
+        s = t.createTransformedShape(s).getBounds();
+        paintImmediately(s.x, s.y, s.width, s.height);
+      } else {
+        paintImmediately(r.tl.x, r.tl.y, r.width(), r.height());
+      }
+    }
+  }
+
+  static final int[] dotcursor_xpm = {
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+    0x00000000, 0xff000000, 0xff000000, 0xff000000, 0x00000000,
+    0x00000000, 0xff000000, 0xff000000, 0xff000000, 0x00000000,
+    0x00000000, 0xff000000, 0xff000000, 0xff000000, 0x00000000,
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  };
+
+  public void setCursor(int width, int height, Point hotspot,
+                        byte[] data)
+  {
+    int i;
+
+    for (i = 0; i < width*height; i++)
+      if (data[i*4 + 3] != 0) break;
+
+    if ((i == width*height) && dotWhenNoCursor.getValue()) {
+      vlog.debug("cursor is empty - using dot");
+      cursor = new BufferedImage(5, 5, BufferedImage.TYPE_INT_ARGB_PRE);
+      cursor.setRGB(0, 0, 5, 5, dotcursor_xpm, 0, 5);
+      cursorHotspot.x = cursorHotspot.y = 3;
+    } else {
+      if ((width == 0) || (height == 0)) {
+        cursor = new BufferedImage(tk.getBestCursorSize(0, 0).width,
+                                   tk.getBestCursorSize(0, 0).height,
+                                   BufferedImage.TYPE_INT_ARGB_PRE);
+        cursorHotspot.x = cursorHotspot.y = 0;
+      } else {
+        IntBuffer buffer = IntBuffer.allocate(width*height);
+        buffer.put(ByteBuffer.wrap(data).asIntBuffer());
+        cursor =
+          new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB_PRE);
+        cursor.setRGB(0, 0, width, height, buffer.array(), 0, width);
+        cursorHotspot = hotspot;
+
+      }
+    }
+
+    int cw = (int)Math.floor((float)cursor.getWidth() * scaleRatioX);
+    int ch = (int)Math.floor((float)cursor.getHeight() * scaleRatioY);
+
+    int x = (int)Math.floor((float)cursorHotspot.x * scaleRatioX);
+    int y = (int)Math.floor((float)cursorHotspot.y * scaleRatioY);
+
+    java.awt.Cursor softCursor;
+
+    Dimension cs = tk.getBestCursorSize(cw, ch);
+    if (cs.width != cw && cs.height != ch) {
+      cw = Math.min(cw, cs.width);
+      ch = Math.min(ch, cs.height);
+      x = (int)Math.min(x, Math.max(cs.width - 1, 0));
+      y = (int)Math.min(y, Math.max(cs.height - 1, 0));
+      BufferedImage scaledImage = 
+        new BufferedImage(cs.width, cs.height, BufferedImage.TYPE_INT_ARGB);
+      Graphics2D g2 = scaledImage.createGraphics();
+      g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                          RenderingHints.VALUE_RENDER_QUALITY);
+      g2.drawImage(cursor,
+                   0, 0, cw, ch,
+                   0, 0, cursor.getWidth(), cursor.getHeight(), null);
+      g2.dispose();
+      java.awt.Point hs = new java.awt.Point(x, y);
+      softCursor = tk.createCustomCursor(scaledImage, hs, "softCursor");
+      scaledImage.flush();
+    } else {
+      java.awt.Point hs = new java.awt.Point(x, y);
+      softCursor = tk.createCustomCursor(cursor, hs, "softCursor");
+    }
+
+    cursor.flush();
+
+    setCursor(softCursor);
+
+  }
+
+  public void resize(int x, int y, int w, int h) {
+    if ((w != frameBuffer.width()) || (h != frameBuffer.height())) {
+      vlog.debug("Resizing framebuffer from "+frameBuffer.width()+"x"+
+                 frameBuffer.height()+" to "+w+"x"+h);
+      frameBuffer = createFramebuffer(frameBuffer.getPF(), w, h);
+      assert(frameBuffer != null);
+      cc.setFramebuffer(frameBuffer);
+      image = (BufferedImage)frameBuffer.getImage();
+    }
+    setScaledSize(w, h);
+  }
+
+  private PlatformPixelBuffer createFramebuffer(PixelFormat pf, int w, int h)
+  {
+    PlatformPixelBuffer fb;
+
+    fb = new JavaPixelBuffer(w, h);
+
+    return fb;
+  }
+
+  //
+  // Callback methods to determine geometry of our Component.
+  //
+
+  public Dimension getPreferredSize() {
+    return new Dimension(scaledWidth, scaledHeight);
+  }
+
+  public Dimension getMinimumSize() {
+    return new Dimension(scaledWidth, scaledHeight);
+  }
+
+  public Dimension getMaximumSize() {
+    return new Dimension(scaledWidth, scaledHeight);
+  }
+
+  public void paintComponent(Graphics g) {
+    Graphics2D g2 = (Graphics2D)g;
+    if (cc.cp.width != scaledWidth ||
+        cc.cp.height != scaledHeight) {
+      g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                          RenderingHints.VALUE_RENDER_QUALITY);
+      g2.drawImage(image, 0, 0, scaledWidth, scaledHeight, null);
+    } else {
+      g2.drawImage(image, 0, 0, null);
+    }
+    g2.dispose();
+  }
+
+  // Mouse-Motion callback function
+  private void mouseMotionCB(MouseEvent e) {
+    if (!viewOnly.getValue() &&
+        e.getX() >= 0 && e.getX() <= scaledWidth &&
+        e.getY() >= 0 && e.getY() <= scaledHeight)
+      cc.writePointerEvent(translateMouseEvent(e));
+  }
+  public void mouseDragged(MouseEvent e) { mouseMotionCB(e); }
+  public void mouseMoved(MouseEvent e) { mouseMotionCB(e); }
+
+  // Mouse callback function
+  private void mouseCB(MouseEvent e) {
+    if (!viewOnly.getValue())
+      if ((e.getID() == MouseEvent.MOUSE_RELEASED) ||
+          (e.getX() >= 0 && e.getX() <= scaledWidth &&
+           e.getY() >= 0 && e.getY() <= scaledHeight))
+        cc.writePointerEvent(translateMouseEvent(e));
+  }
+  public void mouseReleased(MouseEvent e) { mouseCB(e); }
+  public void mousePressed(MouseEvent e) { mouseCB(e); }
+  public void mouseClicked(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {
+    if (embed.getValue())
+      requestFocus();
+  }
+  public void mouseExited(MouseEvent e) {}
+
+  // MouseWheel callback function
+  private void mouseWheelCB(MouseWheelEvent e) {
+    if (!viewOnly.getValue())
+      cc.writeWheelEvent(e);
+  }
+
+  public void mouseWheelMoved(MouseWheelEvent e) {
+    mouseWheelCB(e);
+  }
+
+  private static final Integer keyEventLock = 0; 
+
+  // Handle the key-typed event.
+  public void keyTyped(KeyEvent e) { }
+
+  // Handle the key-released event.
+  public void keyReleased(KeyEvent e) {
+    synchronized(keyEventLock) {
+      cc.writeKeyEvent(e);
+    }
+  }
+
+  // Handle the key-pressed event.
+  public void keyPressed(KeyEvent e)
+  {
+    if (e.getKeyCode() == MenuKey.getMenuKeyCode()) {
+      java.awt.Point pt = e.getComponent().getMousePosition();
+      if (pt != null) {
+        F8Menu menu = new F8Menu(cc);
+        menu.show(e.getComponent(), (int)pt.getX(), (int)pt.getY());
+      }
+      return;
+    }
+    int ctrlAltShiftMask = Event.SHIFT_MASK | Event.CTRL_MASK | Event.ALT_MASK;
+    if ((e.getModifiers() & ctrlAltShiftMask) == ctrlAltShiftMask) {
+      switch (e.getKeyCode()) {
+        case KeyEvent.VK_A:
+          VncViewer.showAbout(this);
+          return;
+        case KeyEvent.VK_F:
+          if (cc.desktop.fullscreen_active())
+            cc.desktop.fullscreen_on();
+          else
+            cc.desktop.fullscreen_off();
+          return;
+        case KeyEvent.VK_H:
+          cc.refresh();
+          return;
+        case KeyEvent.VK_I:
+          cc.showInfo();
+          return;
+        case KeyEvent.VK_O:
+            OptionsDialog.showDialog(this);
+          return;
+        case KeyEvent.VK_W:
+          VncViewer.newViewer();
+          return;
+        case KeyEvent.VK_LEFT:
+        case KeyEvent.VK_RIGHT:
+        case KeyEvent.VK_UP:
+        case KeyEvent.VK_DOWN:
+          return;
+      }
+    }
+    if ((e.getModifiers() & Event.META_MASK) == Event.META_MASK) {
+      switch (e.getKeyCode()) {
+        case KeyEvent.VK_COMMA:
+        case KeyEvent.VK_N:
+        case KeyEvent.VK_W:
+        case KeyEvent.VK_I:
+        case KeyEvent.VK_R:
+        case KeyEvent.VK_L:
+        case KeyEvent.VK_F:
+        case KeyEvent.VK_Z:
+        case KeyEvent.VK_T:
+          return;
+      }
+    }
+    synchronized(keyEventLock) {
+      cc.writeKeyEvent(e);
+    }
+  }
+
+  public void setScaledSize(int width, int height)
+  {
+    assert(width != 0 && height != 0);
+    String scaleString = scalingFactor.getValue();
+    if (remoteResize.getValue()) {
+      scaledWidth = width;
+      scaledHeight = height;
+      scaleRatioX = 1.00f;
+      scaleRatioY = 1.00f;
+    } else {
+      if (scaleString.matches("^[0-9]+$")) {
+        int scalingFactor = Integer.parseInt(scaleString);
+        scaledWidth =
+          (int)Math.floor((float)width * (float)scalingFactor/100.0);
+        scaledHeight =
+          (int)Math.floor((float)height * (float)scalingFactor/100.0);
+      } else if (scaleString.equalsIgnoreCase("Auto")) {
+        scaledWidth = width;
+        scaledHeight = height;
+      } else {
+        float widthRatio = (float)width / (float)cc.cp.width;
+        float heightRatio = (float)height / (float)cc.cp.height;
+        float ratio = Math.min(widthRatio, heightRatio);
+        scaledWidth = (int)Math.floor(cc.cp.width * ratio);
+        scaledHeight = (int)Math.floor(cc.cp.height * ratio);
+      }
+      scaleRatioX = (float)scaledWidth / (float)cc.cp.width;
+      scaleRatioY = (float)scaledHeight / (float)cc.cp.height;
+    }
+    if (scaledWidth != getWidth() || scaledHeight != getHeight())
+      setSize(new Dimension(scaledWidth, scaledHeight));
+  }
+
+  private MouseEvent translateMouseEvent(MouseEvent e)
+  {
+    if (cc.cp.width != scaledWidth ||
+        cc.cp.height != scaledHeight) {
+      int sx = (scaleRatioX == 1.00) ?
+        e.getX() : (int)Math.floor(e.getX() / scaleRatioX);
+      int sy = (scaleRatioY == 1.00) ?
+        e.getY() : (int)Math.floor(e.getY() / scaleRatioY);
+      e.translatePoint(sx - e.getX(), sy - e.getY());
+    }
+    return e;
+  }
+
+  public void handleOptions()
+  {
+    /*
+    setScaledSize(cc.cp.width, cc.cp.height);
+    if (!oldSize.equals(new Dimension(scaledWidth, scaledHeight))) {
+    // Re-layout the DesktopWindow when the scaled size changes.
+    // Ideally we'd do this with a ComponentListener, but unfortunately
+    // sometimes a spurious resize event is triggered on the viewport
+    // when the DesktopWindow is manually resized via the drag handles.
+    if (cc.desktop != null && cc.desktop.isVisible()) {
+      JScrollPane scroll = (JScrollPane)((JViewport)getParent()).getParent();
+      scroll.setViewportBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+      cc.desktop.pack();
+    }
+    */
+  }
+
+  // access to cc by different threads is specified in CConn
+  private CConn cc;
+  private BufferedImage image;
+
+  // access to the following must be synchronized:
+  public PlatformPixelBuffer frameBuffer;
+
+  static Toolkit tk = Toolkit.getDefaultToolkit();
+
+  public int scaledWidth = 0, scaledHeight = 0;
+  float scaleRatioX, scaleRatioY;
+
+  BufferedImage cursor;
+  Point cursorHotspot = new Point();
+
+}
